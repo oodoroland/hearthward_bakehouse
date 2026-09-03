94eda675bdbe541d051213ddc9f953d02cb9c3a2
oodoroland <williamsroland147@gmail.com>
2026-09-04 00:30:44 +0100

    upadte

--- components/layout/nav-links.ts
+++ components/layout/nav-links.ts
@@ -7,8 +7,12 @@ export type NavLink = {
 export const NAV_LINKS: NavLink[] = [
   { label: "Home", path: "home", href: "/" },
   { label: "Today's Counter", path: "todays-counter", href: "/todays-counter" },
-  { label: "Pre-order", path: "pre-order", href: "/pre-order" },
+  // { label: "Pre-order", path: "pre-order", href: "/pre-order" },
   { label: "Visit", path: "visit", href: "/visit" },
-  { label: "Celebrations & Office", path: "celebrations-office", href: "#" },
-  { label: "Our Story", path: "our-story", href: "#" },
+  {
+    label: "Celebrations & Office",
+    path: "celebrations-office",
+    href: "/celebrations-office",
+  },
+  // { label: "Our Story", path: "our-story", href: "#" },
 ];
